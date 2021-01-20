07424e4c9ae9480fc2b51be7e1a1dd57afc68429
Jay Bryant <jbryant@pivotal.io>
2021-01-20 14:33:14 -0600

    Upgrade to Boot 2.4.2
    Keeping the build files current and adding deep links to the Intializr.
    This one included modifying code to account for breaking changes, too.

--- complete/src/test/java/com/example/accessingdatar2dbc/CustomerRepositoryTests.java
+++ complete/src/test/java/com/example/accessingdatar2dbc/CustomerRepositoryTests.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
-import org.springframework.data.r2dbc.core.DatabaseClient;
+import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.r2dbc.dialect.H2Dialect;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
@@ -24,8 +26,8 @@ public class CustomerRepositoryTests {
     @Test
     public void testFindByLastName() {
         Customer customer = new Customer("first", "last");
-        databaseClient.insert().into(Customer.class).using(customer).then().as(StepVerifier::create).verifyComplete();
-
+        R2dbcEntityTemplate template = new R2dbcEntityTemplate(databaseClient, H2Dialect.INSTANCE);
+        template.insert(Customer.class).using(customer).then().as(StepVerifier::create).verifyComplete();
         Flux<Customer> findByLastName = customers.findByLastName(customer.getLastName());
 
 
