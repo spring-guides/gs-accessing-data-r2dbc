5deb682bca187079b49e94f4fce130017bfacaad
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2021-02-02 20:32:19 +0100

    Use the classes from package org.springframework.r2dbc.connection instead of deprecated classes of org.springframework.data.r2dbc.connectionfactory

--- complete/src/main/java/com/example/accessingdatar2dbc/AccessingDataR2dbcApplication.java
+++ complete/src/main/java/com/example/accessingdatar2dbc/AccessingDataR2dbcApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
-import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 import java.time.Duration;
 import java.util.Arrays;
