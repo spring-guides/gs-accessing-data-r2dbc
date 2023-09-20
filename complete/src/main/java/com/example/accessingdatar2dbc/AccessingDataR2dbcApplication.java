a8a56f4d3f389bb787de8e1de5bfe7ee1533cb31
Jay Bryant <jbryant@vmware.com>
2023-09-20 14:51:29 -0500

    Upgraded to Spring Boot 3.1.3.
    Had to upgrade the Gradle wrappers to get it to work.
    Also removed the boot version from the Initializr link.

--- complete/src/main/java/com/example/accessingdatar2dbc/AccessingDataR2dbcApplication.java
+++ complete/src/main/java/com/example/accessingdatar2dbc/AccessingDataR2dbcApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
-import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 import java.time.Duration;
 import java.util.Arrays;
@@ -21,16 +21,6 @@ public class AccessingDataR2dbcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccessingDataR2dbcApplication.class, args);
-    }
-
-    @Bean
-    ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
-
-        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
-        initializer.setConnectionFactory(connectionFactory);
-        initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
-
-        return initializer;
     }
 
     @Bean
